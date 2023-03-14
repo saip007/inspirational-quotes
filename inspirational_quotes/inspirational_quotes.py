8641f03bbdfb8970319e2fc9a6d325c8d0e635aa
saip007 <saip4622@gmail.com>
2023-03-14 16:00:05 +0530

    updated path && changes

--- inspirational_quotes/inspirational_quotes.py
+++ inspirational_quotes/inspirational_quotes.py
@@ -1,7 +1,10 @@
 import json,random
+from os import path
 
 def quote():
-    _quotes = json.loads(open('./quotes/quotes.json', 'rb').read())
+    _path = path.dirname(__file__)
+    _path = path.join(_path,"quotes/quotes.json")
+    _quotes = json.loads(open(_path, 'rb').read())
     _quote = _quotes[random.randint(0,len(_quotes)-1)]
     return (_quote['text'],_quote['from'])
 
