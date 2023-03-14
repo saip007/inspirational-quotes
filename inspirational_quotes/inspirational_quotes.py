626c2da31cc2b928ca065240391c97214ff0f2db
saip007 <saip4622@gmail.com>
2023-03-14 13:07:34 +0530

    adding path

--- inspirational_quotes/inspirational_quotes.py
+++ inspirational_quotes/inspirational_quotes.py
@@ -1,7 +1,7 @@
 import json,random
 
 def quote():
-    _quotes = json.loads(open('inspirational_quotes\inspirational_quotes\quotes\quotes.json', 'rb').read())
+    _quotes = json.loads(open('inspirational_quotes/inspirational_quotes/quotes/quotes.json', 'rb').read())
     _quote = _quotes[random.randint(0,len(_quotes)-1)]
     return (_quote['text'],_quote['from'])
 
