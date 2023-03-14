8fa65d9a0a8a0cc21356672a18aec4446ab1e555
saip007 <saip4622@gmail.com>
2023-03-14 15:00:34 +0530

    updating setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="inspirational_quotes",
-    version="0.0.2",
+    version="0.0.3",
     author="saip007",
     author_email="saip4622@outlook.com",
     description="inspirational quotes package",
