0fbee5b369310ec3c629df4f500bcecb383a2903
Sai Prashanth <saip4622@gmail.com>
2024-12-06 15:53:25 +0530

    Update inspirational_quotes.py

--- inspirational_quotes/inspirational_quotes.py
+++ inspirational_quotes/inspirational_quotes.py
@@ -3,7 +3,7 @@ from os import path
 
 def quote():
     dirpath = path.dirname(__file__)
-    dirpath = path.join(dirpath,"quotes/quotes.json")
+    dirpath = path.join(dirpath,"quotes.json")
     quotes = json.loads(open(dirpath, 'rb').read())
     quote = quotes[random.randint(0,len(quotes)-1)]
     result = {'quote':quote['text'],"author":quote['from']}
